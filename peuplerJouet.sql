bf6e0476c89b97375533bb55f01a1b4baa2063d0
devadmin <devadmin@dev-web.BTSSIO.local>
2024-09-24 14:59:49 +0200

    Création bdd et tables utilisateurs et jouet

---
+++ peuplerJouet.sql
@@ -0,0 +1,6 @@
+use playTimeBDD ;
+
+insert into Jouet (id_jouet, nom_jouet, type_jouet, stock)
+	Values
+	(1,"Blahaj","peluche", "500") ;
+
---
+++ peuplerUtilisateurs.sql
@@ -0,0 +1,6 @@
+use playTimeBDD ;
+
+insert into Utilisateur (id_utilisateur, nom_utilisateur, prenom_utilisateur, email, telephone, adresse, username, mdp)
+	Values
+	(1,"Administrateur","Administrateur","noah.schraen.sio@gmail.com", "1234567891", "69 Avenue St Marie", "Admin", "test") ;
+
