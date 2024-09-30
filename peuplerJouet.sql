e4e2c2307ce0171744ff552af1176f6a9f0adae2
devadmin <devadmin@dev-web.BTSSIO.local>
2024-09-30 09:50:56 +0200

    Remplissage table joeut

--- peuplerJouet.sql
+++
@@ -1,6 +0,0 @@
-use playTimeBDD ;
-
-insert into Jouet (id_jouet, nom_jouet, type_jouet, stock, prix)
-	Values
-	(1,"Blahaj","peluche", "500","25") ;
-
--- peuplerUtilisateurs.sql
+++
@@ -1,6 +0,0 @@
-use playTimeBDD ;
-
-insert into Utilisateur (id_utilisateur, nom_utilisateur, prenom_utilisateur, email, telephone, adresse, username, mdp)
-	Values
-	(1,"Administrateur","Administrateur","noah.schraen.sio@gmail.com", "1234567891", "69 Avenue St Marie", "Admin", "test") ;
-
