99a1c889643cb9687868c853bd8769fbc66e8ef5
devadmin <devadmin@dev-web.BTSSIO.local>
2024-09-30 11:41:16 +0200

    remplissage table jouet

--- creerPlayTimeBDD.sql
+++ creerPlayTimeBDD.sql
@@ -20,6 +20,20 @@ create Table Jouet (
 	nom_jouet varchar(75) ,
 	type_jouet varchar(25),
 	stock int not null,
-	prix int not null
+	prix float not null
 	
 ) ;
+
+create Table Panier (
+	id_panier int primary key not null,
+	article varchar(255),
+	quantité int not null,
+	prix_total int not null,
+	nb_article_total int not null
+);
+
+create Table Avis(
+	id_avis int primary key not null,
+	contenu varchar(280),
+	note float not null
+); 
--- peuplerJouet.sql
+++ peuplerJouet.sql
@@ -2,5 +2,20 @@ use playTimeBDD ;
 
 insert into Jouet (id_jouet, nom_jouet, type_jouet, stock, prix)
 	Values
-	(1,"Blahaj","peluche", "500","25") ;
-
+	(1,"Blahaj","peluche", 336,24.99),
+	(2, "Furbies","peluche", 281, 44.99),
+	(3, "Djungelskog", "peluche", 257, 29.99),
+	(4, "Mogwai", "peluche" , 555, 49.99),
+	(5, "Phoque", "peluche", 214, 24.99),
+	(6, "Annabelle", "poupée", 1, 499.99),
+	(7, "Pack figurines Totally Spies", "figurine", 464, 14.99),
+	(8, "Transformers", "figurine", 523, 19.99),
+	(9, "Power Rangers", "figurine",493, 19.99),
+	(10, "Dragon Ball", "figurine" , 561, 29.99),
+	(11, "Bakugan", "figurine", 367 , 12.99),
+	(12, "Barbie", "poupée", 706, 14.99),
+	(13, "Miraculous", "figurine", 687, 18.99),
+	(14, "Set de cuisine", "set", 120, 159.99),
+	(15, "Set de bricolage", "set", 67, 159.99),
+	(16, "Minicar Garçon", "Voiture", 157, 199.99),
+	(17, "Minicar Fille", "Voiture", 158, 199.99) ;
