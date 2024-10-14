d1ca1f2576f71ecb88afb5c3636a0f35354e4564
NoahSchraen <noah.schraen.sio@gmail.com>
2024-10-14 09:53:18 +0200

    Migrations

---
+++ database/migrations/2024_10_14_073916_create_paniers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaniersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paniers', function (Blueprint $table) {
+            $table->id();
+            $table->string('article');
+            $table->bigInteger('stock');
+            $table->bigInteger('prix');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paniers');
+    }
+}
